7ca3a711bdaa2574a3fc449a266f69fda6d814e8
AbhishekKr <abhikumar163@gmail.com>
2014-09-26 15:42:36 +0530

    utilizing filename time-part to create timeDiv

--- datum-lib/list.sh
+++ datum-lib/list.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+YYYYMMDD_To_TimeDiv(){
+  echo "${@}" | awk -F'-' '{print $2"/"$3"/"$1}' | xargs date +%_d" "%b"'"%Y" *"%A -d
+}
+
 addBlogToList(){
   _json="$1"
   _blog="$2"
@@ -8,18 +12,21 @@ addBlogToList(){
   _fileType=$(echo $_blogFilename | awk -F"." '{print $NF}')
   _blogDate=$(echo $_blogFilename | awk -F"." '{print $1}')
   _blogName=$(echo $_blogFilename | awk -F"." '{print $2}')
+  _timeDiv=$(YYYYMMDD_To_TimeDiv $_blogDate)
 
   if [[ "$_fileType" == "html" ]]; then
     _blogmeta=$(echo $_blog | sed 's/html$/meta/')
     _blogTags=$(cat $_blogmeta | grep '^meta-tags: ' | sed 's/meta-tags: //')
     if [[ "$_blogTags" == "" ]]; then
-      _blogTags="${_blogName}, ${_blogDate}, datum"
+      _blogTags="${_blogName}"
     fi
+    _blogTags="${_blogDate}, ${_blogTags}" #, ${_timeDiv}"
+
     _blogTitle=$(cat $_blogmeta | grep '^meta-title: ' | sed 's/meta-title: //')
     if [[ "$_blogTitle" == "" ]]; then
       _blogTitle="$_blogName"
     fi
-    _blogEntry="  { \"blogTitle\":\"${_blogTitle}\" , \"blogFile\":\"./www-data/blogs/${_blogFilename}\", \"blogTags\":\"${_blogTags}\" },"
+    _blogEntry="  { \"blogTitle\":\"${_blogTitle}\" , \"blogFile\":\"./www-data/blogs/${_blogFilename}\", \"blogTags\":\"${_blogTags}\", \"blogDate\":\"${_timeDiv}\" },"
     echo "${_blogEntry}" >>  "${_json}"
   fi
 }
