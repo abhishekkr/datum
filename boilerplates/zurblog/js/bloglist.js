41c11fbdbbcc5870dd38804e5fa8ef131cea60fa
AbhishekKr <abhikumar163@gmail.com>
2014-11-29 10:11:01 +0530

    fixed empty blogentry, added basicinfo json populated values

--- boilerplates/zurblog/js/bloglist.js
+++ boilerplates/zurblog/js/bloglist.js
@@ -3,11 +3,31 @@ author: AbhishekKr
 occasional javascript artisen, comments/suggestions encouraged
 */
 
+var baseinfo_json_file =  "./www-data/baseinfo.json"
+$.ajax({
+    url: baseinfo_json_file,
+    dataType: 'json',
+    async: false,
+    success: function(json_data) {
+      for(var key in json_data){
+        $("#" + key).text(json_data[key]);
+      }
+    }
+}).fail(function( jqxhr, textStatus, error ) {
+      var err = textStatus + ", " + error;
+      console.log( "Request Failed: " + err );
+});
+
+
+
+/**************** listifying **********************/
+
 function openNew(self){
   window.open(self.innerHTML);
   return false;
 }
 
+var blogEntries = [];
 var options = {
   valueNames: [ 'blogTitle', 'blogFile', 'blogTags', 'blogDate' ],
   item: '<li><article> <a class="blogTitleLink" src="#"><h3 class="blogTitle"></h3></a> <div class="blogDate"></div> <a class="blogFile" href="javascript:void(0)" onclick="openNew(this);" onkeypress="openNew(this);"></a> <p class="blogTags"></p> </article> <hr/></li>',
@@ -15,29 +35,32 @@ var options = {
   plugins: [ ListPagination({}), ListFuzzySearch() ]
 };
 
-var values = [];
-var userList = new List('blogroll', options, values);
 
 /* add all elements from loaded list */
 function update_list(this_item){
-  userList.add({
+  blogEntries.push({
     blogTitle: this_item.blogTitle,
     blogFile: this_item.blogFile,
     blogTags: this_item.blogTags
   });
 }
 
-datum_json =  "./www-data/datum.json"
-$.getJSON( datum_json, {}) .done(function( json ) {
-      $(jQuery.parseJSON(JSON.stringify(json)).reverse()).each(function() {  
+var datum_json_file =  "./www-data/datum.json"
+$.ajax({
+    url: datum_json_file,
+    dataType: 'json',
+    async: false,
+    success: function(json_data) {
+      $(jQuery.parseJSON(JSON.stringify(json_data)).reverse()).each(function() {
           update_list(this)
       });
-  })
-  .fail(function( jqxhr, textStatus, error ) {
+    }
+}).fail(function( jqxhr, textStatus, error ) {
       var err = textStatus + ", " + error;
       console.log( "Request Failed: " + err );
 });
 
+var userList = new List('blogroll', options, blogEntries);
 
 /* tooltip to all titles in list */
 $('*').filter(function() {
