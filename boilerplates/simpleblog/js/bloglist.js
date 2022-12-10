9f7e2bc40f945c8e949379d393f7ac8e4383e445
AbhishekKr <abhikumar163@gmail.com>
2022-12-10 07:28:07 +0530

    [simpleblog] update /blog to show list/content by json & hash
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- boilerplates/simpleblog/js/bloglist.js
+++ boilerplates/simpleblog/js/bloglist.js
@@ -3,94 +3,105 @@ author: AbhishekKr
 occasional javascript artisen, comments/suggestions encouraged
 */
 
+const blogListHash = "list";
+
+const $DOM = function(a,b){var c=document.querySelectorAll(a);if(b===undefined){b=0}return c[b]}
+const loadURI = function(a){var b=new XMLHttpRequest();b.open("GET",a,false);b.send();return b.responseText}
+
+
 function toggleBlogContentToList(){
-    document.location.hash = "homepage";
-    $DOM("#blogcontent").style.display = "none";
-    $DOM("#bloglist").style.display = "block";
+  document.location.hash = blogListHash;
+  $DOM("#blogcontent").style.display = "none";
+  $DOM("#bloglist").style.display = "block";
+  BLOG_MENU.style.display = "none";
 }
+
 
 function blogHashFromLink(blog_link){
-  return blog_link.replace(blog_domain, '')
+  return blog_link.replace(BLOG_DOMAIN, '');
 }
 
+
 function blogLinkFromHash(blog_hash){
-  return blog_domain + blog_hash
+  return BLOG_DOMAIN + blog_hash;
 }
 
 function toggleBlogListToContent(blog_url){
-    var blog_link = window.location.href.split('?')[0] + "?blog=" + encodeURIComponent(blog_url);
+  var blog_link = window.location.href.split('#')[0] + "#" + encodeURIComponent(blog_url);
+  console.log(blog_url);
+  console.log(blog_link);
 
-    var backToMenu = "<div width=\"50%\" style=\"text-align:right;\"><i>";
-    document.location.hash = blogHashFromLink(blog_url);
-    backToMenu += "<span><a href=\"#\" class=\"button radius small\" onClick=\"toggleBlogContentToList();\" onkeypress=\"toggleBlogContentToList();\">back to blog-list</a><span>";
-    backToMenu += "</i></div>";
-
-    var blogContent = loadURI(blog_url);
-    if (undefined != blogContent){
-      blogContent = blogContent + backToMenu;
-      $DOM("#blogcontent").innerHTML = blogContent;
-      $DOM("#blogcontent").style.display = "block";
-      $DOM("#bloglist").style.display = "none";
-    } else {
-      console.log("ERROR: Cannot browse link for this blog.");
-    }
+  document.location.hash = blogHashFromLink(blog_url);
+  var blogContent = loadURI(blog_url);
+  if (undefined != blogContent){
+    console.log(blogcontent);
+    $DOM("#blogcontent").innerHTML = blogContent;
+    $DOM("#blogcontent").style.display = "block";
+    $DOM("#bloglist").style.display = "none";
+    BLOG_MENU.style.display = "block";
+  } else {
+    console.log("ERROR: Cannot browse link for this blog.");
+  }
 }
+
 
 function openBlog(self) {
   var blogFileObject = self.parentNode.getElementsByClassName("blogFile");
   if(blogFileObject.length == 1){
-    toggleBlogListToContent(blogFileObject[0].innerHTML)
+    toggleBlogListToContent(blogFileObject[0].innerHTML);
   } else {
     console.log("ERROR: No link found for this.");
   }
 }
 
-
-/**************** listifying **********************/
-
-var blog_domain = window.location.origin;
-var currentURL = window.location.href;
-var wwwDataParentURL = currentURL.replace(currentURL.split("/").pop(), "");
-
-var blogEntries = [];
-var fuzzyOptions = { searchClass: "fuzzy-search", location: 0, distance: 100, threshold: 0.4, multiSearch: true };
-var options = {
-  valueNames: [ 'blogTitle', 'blogFile', 'blogTags', 'blogDate' ],
-  item: '<li><article><h3><a class="blogTitle" href="javascript:void(0)" onClick="openBlog(this);" onkeypress="openBlog(this);"></a> <span class="blogFile"></span></h3> <div class="blogDate"></div> <h4><small class="blogTags"></small></h4> </article></li>',
-  page: 5,
-  plugins: [ ListPagination({}), ListFuzzySearch() ]
-};
-
-
 /* add all elements from loaded list */
-function update_list(this_item){
-  var blogURL =  wwwDataParentURL + this_item.blogFile;
-  blogURL = blogURL.replace("/./", "/");
-  blogEntries.push({
-    blogTitle: this_item.blogTitle,
-    blogFile: blogURL,
-    blogTags: this_item.blogTags
-  });
+function updateList(item){
+  if ('content' in document.createElement('template') == false) {
+    console.log("ERROR: Failed appending to bloglist: ", item);
+    return
+  }
+  item.blogFile = item.blogFile.replace(/^.\//, '/')
+  const blogURL =  wwwDataParentUrl + '#' + item.blogFile;
+
+  var template = document.getElementById('bloglistTmpl');
+  var blogUL = document.getElementById("bloglist");
+
+  var clone = template.content.cloneNode(true);
+  clone.querySelector(".blogTitle").textContent = item.blogTitle;
+  clone.querySelector(".blogFile").textContent = item.blogFile;
+  clone.querySelector(".blogURL").textContent = blogURL;
+  clone.querySelector(".blogTags").textContent = item.blogTags;
+  blogUL.appendChild(clone);
+  //console.log(blogURL);
 }
 
-var datum_json_file =  "./datum.json"
-$.ajax({
-    url: datum_json_file,
-    dataType: 'json',
-    async: false,
-    success: function(json_data) {
-      $(jQuery.parseJSON(JSON.stringify(json_data)).reverse()).each(function() {
-          update_list(this)
-      });
-    }
-}).fail(function( jqxhr, textStatus, error ) {
-      var err = textStatus + ", " + error;
-      console.log( "Request Failed: " + err );
-});
 
-var userList = new List('blogroll', options, blogEntries);
+const loadDatumJSON = function(jsonfile) {
+  fetch(jsonfile)
+    .then((response) => response.text())
+    .then((data) => {
+        var bloglist = $.parseJSON(data);
+        //console.log(bloglist);
+        bloglist.forEach(function(b) {
+            updateList(b);
+        });
+    });
+}
+
+
+/****************/
+
+const BLOG_DOMAIN = window.location.origin;
+const CURRENT_URL = window.location.href;
+const wwwDataParentUrl = CURRENT_URL.split('#')[0];
+
+var BLOG_MENU = document.getElementById("blogmenu");
+
+const datumJsonFile =  "./datum.json";
+loadDatumJSON(datumJsonFile);
 
 /* tooltip to all titles in list */
+/*
 $('*').filter(function() {
    return $(this).css('text-overflow') == 'ellipsis';
 }).each(function() {
@@ -98,11 +109,11 @@ $('*').filter(function() {
       $(this).attr('blogTitle', $(this).text());
    }
 });
+*/
 
-/****************/
 var blog_hash = document.location.hash.replace(/^#/, '');
-if (blog_hash != "" && blog_hash != "homepage") {
+if (blog_hash != "" && blog_hash != blogListHash) {
   var blog_url = decodeURIComponent(blogLinkFromHash(blog_hash));
-  console.log("Direct Blog Link:", blog_url,blog_hash);
+  console.log("Direct Blog Link:", blog_url, blog_hash);
   toggleBlogListToContent(blog_url);
 }
--- boilerplates/simpleblog/js/simpleblog.js
+++ boilerplates/simpleblog/js/simpleblog.js
@@ -1,14 +1,16 @@
-var blogcontent = document.getElementById('blogcontent');
-var ihtml = undefined;
+/* manages markdown render to html */
 
-fetch('/blogs/2014-11-29.ReadMe.md')
-.then((response) => response.text())
-.then((data) => {
-    //console.log(data);
-  var converter = new showdown.Converter({
-        emoji: true,
-        underline: true,
-      });
-  blogcontent.innerHTML = converter.makeHtml(data);
+const renderMarkdown = function(mduri) {
+  var blogcontent = document.getElementById('blogcontent');
+  var ihtml = undefined;
+
+  fetch(mduri)
+    .then((response) => response.text())
+    .then((data) => {
+      var converter = new showdown.Converter({
+          emoji: true,
+          underline: true,
+        });
+      blogcontent.innerHTML = converter.makeHtml(data);
+    });
 }
-);
